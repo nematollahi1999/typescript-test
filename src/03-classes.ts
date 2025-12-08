1fa751a042a4747ea90aa0438d75eecaa84f385a
Nematollahi <nematollahi.1999@gmail.com>
2025-12-08 09:37:54 +0330

    Fix(03): Classes added.

--- src/03-classes.ts
+++ src/03-classes.ts
@@ -48,31 +48,66 @@ class Dog extends Animal {
 // - abstract method 'getPerimeter()': returns number
 // - protected property 'name': string
 abstract class Shape {
-  // Implement abstract class
+  protected name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+  abstract getArea(): number;
+  abstract getPerimeter(): number;
 }
 
 // TODO: Create a class 'Rectangle' that extends 'Shape' with:
 // - private properties 'width' and 'height': both numbers
 // - implement abstract methods
-class Rectangle {
-  // Extend Shape and implement methods
+class Rectangle extends Shape {
+  private width: number;
+  private height: number;
+
+  constructor(width: number, height: number) {
+    super("Rectangle");
+    this.width = width;
+    this.height = height;
+  }
+
+  public getArea(): number {
+    return this.width * this.height;
+  }
+  public getPerimeter(): number {
+    return 2 * (this.width + this.height);
+  }
 }
 
 // TODO: Create a class 'Circle' that extends 'Shape' with:
 // - private property 'radius': number
 // - implement abstract methods
-class Circle {
-  // Extend Shape and implement methods
+class Circle extends Shape {
+  private radius: number;
+  constructor(radius: number) {
+    super("Circle");
+    this.radius = radius;
+  }
+  public getArea(): number {
+    return 3.14 * this.radius * this.radius;
+  }
+  public getPerimeter(): number {
+    return 2 * 3.14 * this.radius;
+  }
 }
 
 // TODO: Create an interface 'Flyable' with method 'fly()': returns void
 interface Flyable {
-  // Define interface
+  fly(): void;
 }
 
 // TODO: Create a class 'Bird' that extends 'Animal' and implements 'Flyable'
-class Bird {
-  // Extend Animal and implement Flyable
+class Bird extends Animal implements Flyable {
+  constructor(name: string, age: number) {
+    super(name, age);
+  }
+  public fly(): void {
+    console.log(`${this.getName()} is flying!`);
+  }
 }
 
 export { Animal, Dog, Shape, Rectangle, Circle, Flyable, Bird };
