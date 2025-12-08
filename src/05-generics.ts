198aa99851285d7651d84fadb0c4d2d62f4f4d19
Nematollahi <nematollahi.1999@gmail.com>
2025-12-08 10:34:58 +0330

    Fix(05): Generics added.

--- src/05-generics.ts
+++ src/05-generics.ts
@@ -5,21 +5,18 @@
  */
 
 // TODO: Create a generic function 'identity' that takes a value of type T and returns T
-function identity<T>(value: any): any {
-  // Implement function
+function identity<T>(value: T): T {
   return value;
 }
 
 // TODO: Create a generic function 'getFirst' that takes an array of type T[] and returns T
-function getFirst<T>(array: any): any {
-  // Implement function
+function getFirst<T>(array: T[]): T {
   return array[0];
 }
 
 // TODO: Create a generic function 'merge' that takes two objects and returns a merged object
 // Use intersection types in the return type
-function merge<T, U>(obj1: any, obj2: any): any {
-  // Implement function
+function merge<T, U>(obj1: T, obj2: U): T & U {
   return { ...obj1, ...obj2 };
 }
 
@@ -28,7 +25,9 @@ function merge<T, U>(obj1: any, obj2: any): any {
 // - getAll(): T[]
 // - save(item: T): void
 interface Repository<T> {
-  // Define interface methods
+  getById(id: number): T;
+  getAll(): T[];
+  save(item: T): void;
 }
 
 // TODO: Create a generic class 'Stack' with:
@@ -37,24 +36,31 @@ interface Repository<T> {
 // - pop(): T | undefined
 // - isEmpty(): boolean
 class Stack<T> {
-  // Implement class
+  private items: T[] = [];
+  push(item: T): void {
+    this.items.push(item);
+  }
+  pop(): T | undefined {
+    return this.items.pop();
+  }
+  isEmpty(): boolean {
+    return this.items.length === 0;
+  }
 }
 
 // TODO: Create a generic function 'filterArray' that:
 // - takes an array of type T[]
 // - takes a predicate function (item: T) => boolean
 // - returns filtered array of type T[]
-function filterArray<T>(array: any, predicate: any): any {
-  // Implement function
-  return [];
+function filterArray<T>(array: T[], predicate: (item: T) => boolean): T[] {
+  return array.filter(predicate);
 }
 
 // TODO: Create a generic type constraint function 'getProperty' that:
 // - takes an object of type T
 // - takes a key of type K where K extends keyof T
 // - returns T[K]
-function getProperty<T, K extends keyof T>(obj: any, key: any): any {
-  // Implement function
+function getProperty<T, K extends keyof T>(obj: T, key: K): T[K] {
   return obj[key];
 }
 
@@ -64,7 +70,18 @@ function getProperty<T, K extends keyof T>(obj: any, key: any): any {
 // - getFirst(): T
 // - getSecond(): U
 class Pair<T, U> {
-  // Implement class
+  private first: T;
+  private second: U;
+  constructor(first: T, second: U) {
+    this.first = first;
+    this.second = second;
+  }
+  getFirst(): T {
+    return this.first;
+  }
+  getSecond(): U {
+    return this.second;
+  }
 }
 
 export { identity, getFirst, merge, Repository, Stack, filterArray, getProperty, Pair };
